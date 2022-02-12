6bb54109bf3dab0642278a5a52f04e10f4f5c2c2
Orkusha <orkusha.d@gmail.com>
2022-02-12 16:45:51 +0200

    just to commit

---
+++ DimonCocoa/Classes/DimonCocoa.swift
@@ -0,0 +1,18 @@
+//
+//  DimonCocoa.swift
+//  DimonCocoa
+//
+//  Created by mac on 12.02.2022.
+//
+
+import Foundation
+
+public struct DimonCocoa {
+    
+    private let text = "Hello, i'm Cocoa Pod"
+    
+    public func sayHello() {
+        print(text)
+    }
+    
+}
